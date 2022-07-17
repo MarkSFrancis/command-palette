775c6a0ff1b4530505ea90eb1b13bcd705422925
MarkSFrancis <mrkfrancis8@gmail.com>
2022-07-17 20:52:24 +0100

    Fix failing tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   setupFilesAfterEnv: ["<rootDir>/setup-tests.ts"],
+  testEnvironment: "jsdom",
   globals: {
     "ts-jest": {
       tsconfig: "tsconfig.test.json",
